00f186c5ec2b6e35fd1dc7bc35325af612b979c3
pavlo <pavelnemoi@gmail.com>
2019-04-10 17:34:08 +0300

    Implement getUserCollection action, add paginatedResponse to api ctrl

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -4,14 +4,34 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\Action\GetCollectionRequest;
+use App\Action\User\GetUserCollectionAction;
 use App\Http\Controllers\ApiController;
+use App\Http\Presenter\User\UserArrayPresenter;
+use App\Http\Request\Api\CollectionHttpRequest;
 use App\Http\Response\ApiResponse;
-use Illuminate\Http\Request;
 
 final class UserController extends ApiController
 {
-    public function getUserCollection(Request $request): ApiResponse
+    private $getUserCollectionAction;
+    private $presenter;
+
+    public function __construct(GetUserCollectionAction $getUserCollectionAction, UserArrayPresenter $presenter)
     {
-        return $this->createSuccessResponse();
+        $this->getUserCollectionAction = $getUserCollectionAction;
+        $this->presenter = $presenter;
+    }
+
+    public function getUserCollection(CollectionHttpRequest $request): ApiResponse
+    {
+        $response = $this->getUserCollectionAction->execute(
+            new GetCollectionRequest(
+                (int)$request->query('page'),
+                $request->query('sort'),
+                $request->query('direction')
+            )
+        );
+
+        return $this->createPaginatedResponse($response->getPaginator(), $this->presenter);
     }
 }
