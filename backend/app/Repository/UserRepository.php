255a64993f73680507bad38a347aa82e48237697
pavlo <pavelnemoi@gmail.com>
2019-04-10 17:30:09 +0300

    User repository add paginate method

--- backend/app/Repository/UserRepository.php
+++ backend/app/Repository/UserRepository.php
@@ -5,11 +5,21 @@ declare(strict_types = 1);
 namespace App\Repository;
 
 use App\Entity\User;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 final class UserRepository
 {
     public function create(array $fields): User
     {
         return User::create($fields);
+    }
+
+    public function paginate(
+        int $page = self::DEFAULT_PAGE,
+        int $perPage = self::DEFAULT_PER_PAGE,
+        string $sort = self::DEFAULT_SORT,
+        string $direction = self::DEFAULT_DIRECTION
+    ): LengthAwarePaginator {
+        return User::query()->orderBy($sort, $direction)->paginate($perPage, ['*'], null, $page);
     }
 }
