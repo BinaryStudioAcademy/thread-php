4ac9ec00b0ece5d714f40dfd80363b4b61b6fd29
pavlo <pavelnemoi@gmail.com>
2019-04-10 18:10:49 +0300

    Implement get user by id logic

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\Action\GetByIdRequest;
 use App\Action\GetCollectionRequest;
+use App\Action\User\GetUserByIdAction;
 use App\Action\User\GetUserCollectionAction;
 use App\Http\Controllers\ApiController;
 use App\Http\Presenter\User\UserArrayPresenter;
@@ -15,11 +17,16 @@ final class UserController extends ApiController
 {
     private $getUserCollectionAction;
     private $presenter;
+    private $getUserByIdAction;
 
-    public function __construct(GetUserCollectionAction $getUserCollectionAction, UserArrayPresenter $presenter)
-    {
+    public function __construct(
+        GetUserCollectionAction $getUserCollectionAction,
+        UserArrayPresenter $presenter,
+        GetUserByIdAction $getUserByIdAction
+    ) {
         $this->getUserCollectionAction = $getUserCollectionAction;
         $this->presenter = $presenter;
+        $this->getUserByIdAction = $getUserByIdAction;
     }
 
     public function getUserCollection(CollectionHttpRequest $request): ApiResponse
@@ -33,5 +40,12 @@ final class UserController extends ApiController
         );
 
         return $this->createPaginatedResponse($response->getPaginator(), $this->presenter);
+    }
+
+    public function getUserById(string $id): ApiResponse
+    {
+        $user = $this->getUserByIdAction->execute(new GetByIdRequest((int)$id))->getUser();
+
+        return $this->createSuccessResponse($this->presenter->present($user));
     }
 }
