61b3288a53c31ba8c1017d5c1b8571fdbbb5f843
Oleksandr Strakhov <mrallexus@gmail.com>
2019-04-24 14:17:06 +0300

    Install Buefy

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
