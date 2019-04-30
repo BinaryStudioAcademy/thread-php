21ace7452745bd0c2a85734ba4e4304aa0cf2d69
pavlo <pavelnemoi@gmail.com>
2019-04-30 19:15:18 +0300

    Create root state vuex files, add loading mutation

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import state from './store/state';
+import mutations from './store/mutations';
+import getters from './store/getters';
+import actions from './store/actions';
 import auth from './store/modules/auth';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     strict: process.env.NODE_ENV !== 'production',
-    state: {},
-    mutations: {},
-    actions: {},
+    state,
+    mutations,
+    actions,
+    getters,
     modules: {
         auth,
     },
