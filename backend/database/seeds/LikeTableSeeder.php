e89940e0aa26d7334e1076d13fffef22130c49e0
pavlo <pavelnemoi@gmail.com>
2019-05-12 21:29:55 +0300

    Edit like seeder logic

--- backend/database/seeds/LikeTableSeeder.php
+++ backend/database/seeds/LikeTableSeeder.php
@@ -4,13 +4,13 @@ use App\Entity\Comment;
 use App\Entity\Like;
 use App\Entity\Tweet;
 use App\Entity\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class LikeTableSeeder extends Seeder
 {
-    private const MIN_LIKES_COUNT = 1;
-    private const MAX_LIKES_COUNT = 5;
+    private const LIKES_COUNT = 10;
 
     /**
      * Run the database seeds.
@@ -22,31 +22,47 @@ class LikeTableSeeder extends Seeder
         $tweets = Tweet::all();
         $users = User::all();
         $comments = Comment::all();
+        $now = Carbon::now();
 
         $tweetLikes = $tweets->map(
-            function(Tweet $tweet) use($users) {
-                $likesCount = random_int(self::MIN_LIKES_COUNT, self::MAX_LIKES_COUNT);
+            function(Tweet $tweet) use($users, $now) {
+                $userIds = $users->shuffle()->shuffle()->take(self::LIKES_COUNT)->pluck('id');
 
-                return factory(Like::class, $likesCount)->make([
-                    'user_id' => $users->random()->id,
-                    'likeable_id' => $tweet->id,
-                    'likeable_type' => Tweet::class,
-                ]);
+                return $userIds->map(
+                    function(int $userId) use($tweet, $now) {
+                        return [
+                            'user_id' => $userId,
+                            'likeable_id' => $tweet->id,
+                            'likeable_type' => Tweet::class,
+                            'created_at' => $now->toDateTimeString()
+                        ];
+                    }
+                );
             }
         );
 
         $commentLikes = $comments->map(
-            function(Comment $comment) use($users) {
-                $likesCount = random_int(self::MIN_LIKES_COUNT, self::MAX_LIKES_COUNT);
+            function(Comment $comment) use($users, $now) {
+                $userIds = $users->shuffle()->shuffle()->take(self::LIKES_COUNT)->pluck('id');
 
-                return factory(Like::class, $likesCount)->make([
-                    'user_id' => $users->random()->id,
-                    'likeable_id' => $comment->id,
-                    'likeable_type' => Comment::class,
-                ]);
+                return $userIds->map(
+                    function(int $userId) use($comment, $now) {
+                        return [
+                            'user_id' => $userId,
+                            'likeable_id' => $comment->id,
+                            'likeable_type' => Comment::class,
+                            'created_at' => $now->toDateTimeString()
+                        ];
+                    }
+                );
             }
         );
 
-        DB::table('likes')->insert($tweetLikes->flatten()->merge($commentLikes->flatten())->toArray());
+        DB::table('likes')->insert(
+            $tweetLikes
+                ->flatten(1)
+                ->merge($commentLikes->flatten(1))
+                ->toArray()
+        );
     }
 }
