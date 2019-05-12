ab59bcf570d91d07ca057f62f77b4e4a6c9df3a5
pavlo <pavelnemoi@gmail.com>
2019-05-12 16:19:07 +0300

    Fix tweet mapper function

--- frontend/src/services/Normalizer.js
+++ frontend/src/services/Normalizer.js
@@ -21,5 +21,5 @@ export const tweetMapper = tweet => ({
     imageUrl: tweet.image_url,
     created: tweet.created_at,
     author: userMapper(tweet.author),
-    comments: tweet.comments.map(commentMapper)
+    commentsCount: tweet.comments_count
 });
