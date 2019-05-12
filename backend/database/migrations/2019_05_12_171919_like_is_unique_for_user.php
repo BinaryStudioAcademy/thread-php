1bf7f2b27d7719342a636415bb59c04230a8622e
pavlo <pavelnemoi@gmail.com>
2019-05-12 21:29:42 +0300

    Add unique like type migration

---
+++ backend/database/migrations/2019_05_12_171919_like_is_unique_for_user.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LikeIsUniqueForUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // user can like smth only once
+        Schema::table('likes', function(Blueprint $blueprint) {
+            $blueprint->unique(['user_id', 'likeable_id', 'likeable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('likes', function(Blueprint $blueprint) {
+            // need to drop foreign key before we can drop unique index
+            $blueprint->dropForeign('likes_user_id_foreign');
+            $blueprint->dropUnique(['user_id', 'likeable_id', 'likeable_type']);
+            $blueprint
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+}
