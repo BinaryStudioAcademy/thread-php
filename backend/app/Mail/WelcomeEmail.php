aa08b7c23951b6106e72632d1c0d58ca4db7838b
Oleksandr Strakhov <mrallexus@gmail.com>
2019-05-13 16:50:27 +0300

    Fix remarks

--- backend/app/Mail/WelcomeEmail.php
+++ backend/app/Mail/WelcomeEmail.php
@@ -12,22 +12,12 @@ class WelcomeEmail extends Mailable implements ShouldQueue
     use Queueable, SerializesModels;
 
     /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Build the message.
      *
      * @return $this
      */
     public function build()
     {
-        return $this->view('emails.welcome');
+        return $this->subject('Welcome!')->view('emails.welcome');
     }
 }
